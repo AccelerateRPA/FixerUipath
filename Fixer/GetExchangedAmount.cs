170c32e4bd2be63faf9dddd1f9a5003b8bd0c5ec
Elliott <51993605+EllBot@users.noreply.github.com>
2019-06-20 11:24:49 +0100

    1st Upload
    Activities:
    Get present exchange rate
    Get exchange rate for a specified datetime
    Exchange a present amount
    Exchange an amount for a specified datetime

---
+++ Fixer/GetExchangedAmount.cs
@@ -0,0 +1,44 @@
+﻿using System.Activities;
+using System.ComponentModel;
+using FixerSharp;
+
+namespace FixerIO.Workflow.Activities
+{
+    public class GetExchangedAmount : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        [Description("The amount you wish to convert into a different currency")]
+        public InArgument<double> InputAmount { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        [Description("The original currency you wish to convert. Please enter as a string of the currency code, e.g. $=USD. A full list can be found here: https://fixer.io/symbols")]
+        public InArgument<string> InputCurrency { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        [Description("The currency to be converted to. Please enter as a string of the currency code, e.g. $=USD. A full list can be found here: https://fixer.io/symbols")]
+        public InArgument<string> OutputCurrency { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        [Description("Your Fixer.IO Account APIKey.  This can be found in your Fixer.IO dashboard: https://fixer.io/dashboard")]
+        public InArgument<string> APIKey { get; set; }
+
+        [Category("Output")]
+        [Description("The amount after conversion.")]
+        public OutArgument<double> OutputAmount { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var aPIKey = APIKey.Get(context);
+            Fixer.SetApiKey(aPIKey);
+            var inputAmount = InputAmount.Get(context);
+            var inputCurrency = InputCurrency.Get(context);
+            var outputCurrency = OutputCurrency.Get(context);
+            double outputAmount = Fixer.Convert(inputCurrency, outputCurrency, inputAmount);
+            OutputAmount.Set(context, outputAmount);
+        }
+    }
+}
---
+++ Fixer/GetPastExchangeRate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Activities;
+using System.ComponentModel;
+using FixerSharp;
+
+namespace FixerIO.Workflow.Activities
+{
+    public class GetPastExchangeRate : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        [Description("The original currency you wish to convert. Please enter as a string of the currency code, e.g. $=USD. A full list can be found here: https://fixer.io/symbols")]
+        public InArgument<string> InputCurrency { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        [Description("The currency to be converted to. Please enter as a string of the currency code, e.g. $=USD. A full list can be found here: https://fixer.io/symbols")]
+        public InArgument<string> OutputCurrency { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        [Description("Your Fixer.IO Account APIKey.  This can be found in your Fixer.IO dashboard: https://fixer.io/dashboard")]
+        public InArgument<string> APIKey { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        [Description("The date you wish to get the exchnage rate from.")]
+        public InArgument<DateTime> PastDate { get; set; }
+
+        [Category("Output")]
+        [Description("The conversion rate between the two specified currencies.")]
+        public OutArgument<double> Rate { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var aPIKey = APIKey.Get(context);
+            Fixer.SetApiKey(aPIKey);
+            var inputCurrency = InputCurrency.Get(context);
+            var outputCurrency = OutputCurrency.Get(context);
+            var pastDate = PastDate.Get(context);
+            var rate = Fixer.Rate(inputCurrency, outputCurrency, pastDate);
+            double dblrate = rate.Convert(1);
+            Rate.Set(context, dblrate);
+        }
+    }
+}
